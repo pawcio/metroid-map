47e9a62164252f4e3e155934624473f5d8833667
Paweł Kaczmarz <pawel@kaczmarz.pl>
2016-12-08 21:02:29 +0100

    first test, added mockito to dependencies

---
+++ src/test/java/pl/kwako/metroid_map/ImageCoordinateTranslatorTest.java
@@ -0,0 +1,52 @@
+package pl.kwako.metroid_map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ImageCoordinateTranslatorTest {
+
+    private ImageCoordinateTranslator imageCoordinateTranslator;
+
+    @Mock
+    private Settings settings;
+
+    @Before
+    public void setUp() throws Exception {
+        imageCoordinateTranslator = new ImageCoordinateTranslator(settings);
+        doReturn(3).when(settings).roomImgWidth();
+        doReturn(5).when(settings).roomImgHeight();
+    }
+
+    @Test
+    public void zeroXGivesZero() {
+        assertThat(imageCoordinateTranslator.toImageX(0), is(0));
+    }
+
+    @Test
+    public void imageXMultipliesByRoomWidth() {
+        int imageX = imageCoordinateTranslator.toImageX(2);
+        verify(settings).roomImgWidth();
+        assertThat(imageX, is(2 * 3));
+    }
+
+    @Test
+    public void zeroYGivesZero() {
+        assertThat(imageCoordinateTranslator.toImageY(0), is(0));
+    }
+
+    @Test
+    public void imageYMultipliesByRoomHeight() {
+        int imageX = imageCoordinateTranslator.toImageY(2);
+        verify(settings).roomImgHeight();
+        assertThat(imageX, is(2 * 5));
+    }
+}
